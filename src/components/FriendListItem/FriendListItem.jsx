4b34b8743bb7f2705c9a8a42055c93f5e943382f
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-03-20 14:16:36 +0200

    ver 1.02.01

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -13,5 +13,4 @@ FriendListItem.propTypes = {
   avatar: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   isOnline: PropTypes.bool.isRequired,
-  id: PropTypes.number.isRequired,
 };
